e425cb025e4712eb3b00cdec80eaddcf157b6d46
xhomerly <jan.lysy@xhomerly.com>
2025-02-22 11:48:36 +0100

    changed images and TODO: udelat Menu a MapLoader

--- src/main/java/com/xhomerly/sokoban/Crate.java
+++ src/main/java/com/xhomerly/sokoban/Crate.java
@@ -2,6 +2,6 @@ package com.xhomerly.sokoban;
 
 public class Crate extends Cell {
     public Crate(int x, int y) {
-        super(x, y, "images/crate_1.png");
+        super(x, y, "images/crate_01.png");
     }
 }
